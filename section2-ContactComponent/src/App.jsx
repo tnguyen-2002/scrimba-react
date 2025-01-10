7d9225767370271b2d7cdf73475581c0f88afaa8
rei666 <h1.tnguyen088@gmail.com>
2025-01-10 14:47:08 +0700

    Passing data into component

--- section2-ContactComponent/src/App.jsx
+++ section2-ContactComponent/src/App.jsx
@@ -3,10 +3,30 @@ import Contact from "./components/Contact";
 export default function App() {
   return (
     <>
-      <Contact />
-      <Contact />
-      <Contact />
-      <Contact />
+      <Contact 
+        img="src/images/shinji-akari.png"
+        name="Shinji Akari"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact 
+        img="src/images/asuka.png"
+        name="Asuka Langley"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact 
+        img="src/images/rei.png"
+        name="Rei Ayanami"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact 
+        img="src/images/kaworu.png"
+        name="Kaworu Nagisa"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
     </>
   )
 }
