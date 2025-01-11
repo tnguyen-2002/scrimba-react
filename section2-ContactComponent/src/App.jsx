5c885846bdcfca3a63d85ed4f9f56b3520263dc9
rei666 <h1.tnguyen088@gmail.com>
2025-01-11 15:13:45 +0700

    props practice

--- section2-ContactComponent/src/App.jsx
+++ section2-ContactComponent/src/App.jsx
@@ -1,32 +1,48 @@
-import Contact from "./components/Contact";
+/**
+ * 
+ * Challenge: create a page that displays your favorite jokes
+ * - Create a Joke component in its own file.
+ * - Import and render 4-5 <Joke /> components
+ * - Each Joke should receive a "setup" prop and a "punchline" prop
+ *   and render those however you'd like
+ * - Use your favorite 2-part jokes (setup & punchline), or check
+ *   jokes.md file for some examples.
+ * 
+ * EXTRA CREDIT:
+ * Some jokes are only a punchline with no setup:
+ * 
+ * E.g.: "It’s hard to explain puns to kleptomaniacs because 
+ * they always take things literally."
+ * 
+ * If you don't pass in a "question" prop, how might you make it only 
+ * show the punchline?
+ */
+
+import Joke from "./components/Joke"
 
 export default function App() {
   return (
-    <>
-      <Contact 
-        img="src/images/shinji-akari.png"
-        name="Shinji Akari"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
-      />
-      <Contact 
-        img="src/images/asuka.png"
-        name="Asuka Langley"
-        phone="(212) 666-1234"
-        email="mr.abcdkaz@catnap.meow"
-      />
-      <Contact 
-        img="src/images/rei.png"
-        name="Rei Ayanami"
-        phone="(212) 777-1234"
-        email="mr.wwwwkaz@catnap.meow"
-      />
-      <Contact 
-        img="src/images/kaworu.png"
-        name="Kaworu Nagisa"
-        phone="(212) 888-1234"
-        email="mr.dfqqaddv@catnap.meow"
-      />
-    </>
+      <>
+        <Joke
+          punchline="It’s hard to explain puns to kleptomaniacs because 
+          * they always take things literally."
+        />
+        <Joke
+          setup="How did the hacker escape the police?"
+          punchline="He just ransomware!"
+        />
+        <Joke
+          setup="Why don't pirates travel on mountain roads?"
+          punchline="Scurvy."
+        />
+        <Joke
+          setup="Why do bees stay in the hive in the winter?"
+          punchline="Swarm."
+        />
+        <Joke
+          setup="What's the best thing about Switzerland?"
+          punchline="I don't know, but the flag is a big plus!"
+        />
+      </>
   )
 }
---
+++ section2-ContactComponent/src/components/Joke.jsx
@@ -0,0 +1,12 @@
+export default function Joke({setup, punchline}) {
+    return (
+        <>
+            {/*  */}
+            {/* {setup && <p className="joke-setup"> Setup: {setup}</p>} */}
+            <p style={{display: setup ? "block" : "none"}}> Setup: {setup}</p>
+
+            <p className="joke-punchline"> Punch line: {punchline}</p>
+            <hr/>
+        </>
+    )
+}
