429dd159b1f338f8a6972213fed00b8c13e57263
rei666 <h1.tnguyen088@gmail.com>
2025-01-15 10:41:31 +0700

    React render array

--- section2-ContactComponent/src/App.jsx
+++ section2-ContactComponent/src/App.jsx
@@ -1,63 +1,17 @@
-/**
- * 
- * Challenge: create a page that displays your favorite jokes
- * - Create a Joke component in its own file.
- * - Import and render 4-5 <Joke /> components
- * - Each Joke should receive a "setup" prop and a "punchline" prop
- *   and render those however you'd like
- * - Use your favorite 2-part jokes (setup & punchline), or check
- *   jokes.md file for some examples.
- * 
- * EXTRA CREDIT:
- * Some jokes are only a punchline with no setup:
- * 
- * E.g.: "It’s hard to explain puns to kleptomaniacs because 
- * they always take things literally."
- * 
- * If you don't pass in a "question" prop, how might you make it only 
- * show the punchline?
- */
-
-import Header from "./components/Header"
-import Entry from "./components/Entry"
+import { ninjaTurtles } from "./data"
 
 export default function App() {
-  return (
-      <>
-        <Header />
-        <main className="entry-container">
-          <Entry
-            img={{
-              src:"https://images.unsplash.com/photo-1589308078059-be1415eab4c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDIwNjJ8MHwxfGFsbHx8fHx8fHx8fDE3MjMyMzUwNjR8&ixlib=rb-4.0.3&q=80&w=1080",
-              alt:"Image of Mount Fuji"
-            }}
-            country="Japan"
-            title="mount fuji"
-            tripDate="12 Jan, 2021 - 24 Jan, 2021"
-            placeDesc="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-          />
-          <Entry
-            img={{
-              src:"https://images.unsplash.com/photo-1589308078059-be1415eab4c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDIwNjJ8MHwxfGFsbHx8fHx8fHx8fDE3MjMyMzUwNjR8&ixlib=rb-4.0.3&q=80&w=1080",
-              alt:"Image of Mount Fuji"
-            }}
-            country="Japan"
-            title="mount fuji"
-            tripDate="12 Jan, 2021 - 24 Jan, 2021"
-            placeDesc="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-          />
-          <Entry
-            img={{
-              src:"https://images.unsplash.com/photo-1589308078059-be1415eab4c3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w2NDIwNjJ8MHwxfGFsbHx8fHx8fHx8fDE3MjMyMzUwNjR8&ixlib=rb-4.0.3&q=80&w=1080",
-              alt:"Image of Mount Fuji"
-            }}
-            country="Japan"
-            title="mount fuji"
-            tripDate="12 Jan, 2021 - 24 Jan, 2021"
-            placeDesc="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-          />
-        </main>
-        
-      </>
+  const listItems = ninjaTurtles.map (nt => 
+    <h2 key = {nt.id}>
+      {nt.name}
+    </h2>
+
+  )
+  return(
+    <>
+      <main>
+        {listItems}
+      </main>
+    </>
   )
 }
---
+++ section2-ContactComponent/src/data.js
@@ -0,0 +1,13 @@
+export const ninjaTurtles = [{
+    id: 0,
+    name: "Donatello"
+}, {
+    id: 1,
+    name: "Michaelangelo"
+}, {
+    id: 2,
+    name: "Rafael"
+}, {
+    id: 3,
+    name: "Leonardo"
+}]
