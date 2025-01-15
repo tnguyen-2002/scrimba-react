df6ac3d26fcda6f526249a540a39da0723142b50
rei666 <h1.tnguyen088@gmail.com>
2025-01-15 22:35:56 +0700

    Chef Claude: Header

---
+++ section3-ReactState/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import './App.css'
+
+export default function App() {
+    return (
+        <>
+            <Header/>
+            
+        </>
+        
+    )
+}
---
+++ section3-ReactState/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <>
+            <header className="header">
+                <img src="src/assets/chef-claude-icon.png" />
+                <p>Chef Claude</p>
+            </header>
+        </>
+    )
+}
