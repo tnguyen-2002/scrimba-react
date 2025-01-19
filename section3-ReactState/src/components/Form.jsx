ba94e9143dc6ca1d6a3812f3958d48e80ad07e06
rei666 <h1.tnguyen088@gmail.com>
2025-01-19 15:12:39 +0700

    Fix form

--- section3-ReactState/src/components/Form.jsx
+++ section3-ReactState/src/components/Form.jsx
@@ -1,13 +1,16 @@
 export default function Form() {
     return (
-        <form className="ingredient-form">
-            <input
-                type="text"
-                aria-label="ingredient"
-                placeholder="e.g. oregano"
-            />
-            <button>+ Add ingredient</button>
+        <main>
+            <form className="ingredient-form">
+                <input
+                    type="text"
+                    aria-label="ingredient"
+                    placeholder="e.g. oregano"
+                />
+            <button>Add ingredient</button>
                 
         </form>
+        </main>
+        
     )
 }
