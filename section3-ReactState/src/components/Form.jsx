4cce3be1e994de8f6eee933b66885bcccee26b2d
rei666 <h1.tnguyen088@gmail.com>
2025-01-23 23:35:14 +0700

    Chef Claude: Refactor array state

--- section3-ReactState/src/components/Form.jsx
+++ section3-ReactState/src/components/Form.jsx
@@ -1,15 +1,38 @@
+import React from 'react'
+
 export default function Form() {
+    const [ingredients, setIngredient] = React.useState([])
+    
+    const ingredientsListItems = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        const capIngredient = `${newIngredient.slice(0,1).toUpperCase()}`+`${newIngredient.slice(1)}`
+        
+        setIngredient(prevIngredientsArray => [...prevIngredientsArray, capIngredient])
+
+        console.log(ingredients)
+    }
+
     return (
         <main>
-            <form className="ingredient-form">
+            <form onSubmit={handleSubmit} className="ingredient-form">
                 <input
                     type="text"
                     aria-label="ingredient"
                     placeholder="e.g. oregano"
+                    name="ingredient"
                 />
             <button>Add ingredient</button>
-                
-        </form>
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
         </main>
         
     )
